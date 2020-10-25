12d2b1fb335d13bc14658cc9f61620e14d3bfcd6
Frode Sjovatsen <frode@fintlabs.no>
2020-10-25 14:18:57 +0100

    added exception handlers in custom controller

--- src/main/java/no/fint/consumer/models/drosjeloyve/DrosjeloyveCustomController.java
+++ src/main/java/no/fint/consumer/models/drosjeloyve/DrosjeloyveCustomController.java
@@ -1,14 +1,18 @@
 package no.fint.consumer.models.drosjeloyve;
 
-import no.fint.consumer.exceptions.EventResponseException;
+import no.fint.cache.exceptions.CacheNotFoundException;
+import no.fint.consumer.exceptions.*;
 import no.fint.consumer.utils.RestEndpoints;
 import no.fint.event.model.HeaderConstants;
 import no.fint.model.resource.arkiv.samferdsel.DrosjeloyveResource;
 import no.fint.relations.FintRelationsMediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.net.UnknownHostException;
 
 @RestController
 @CrossOrigin
@@ -43,6 +47,41 @@ public class DrosjeloyveCustomController {
     @ExceptionHandler(EventResponseException.class)
     public ResponseEntity handleEventResponseException(EventResponseException e) {
         return ResponseEntity.status(e.getStatus()).body(e.getResponse());
+    }
+
+    @ExceptionHandler(UpdateEntityMismatchException.class)
+    public ResponseEntity handleUpdateEntityMismatch(Exception e) {
+        return ResponseEntity.badRequest().body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity handleEntityNotFound(Exception e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(CreateEntityMismatchException.class)
+    public ResponseEntity handleCreateEntityMismatch(Exception e) {
+        return ResponseEntity.badRequest().body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(EntityFoundException.class)
+    public ResponseEntity handleEntityFound(Exception e) {
+        return ResponseEntity.status(HttpStatus.FOUND).body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(CacheDisabledException.class)
+    public ResponseEntity handleBadRequest(Exception e) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(UnknownHostException.class)
+    public ResponseEntity handleUnkownHost(Exception e) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(ErrorResponse.of(e));
+    }
+
+    @ExceptionHandler(CacheNotFoundException.class)
+    public ResponseEntity handleCacheNotFound(Exception e) {
+        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE).body(ErrorResponse.of(e));
     }
 
 }
