1acfb5918b12feabaf141f433bce1a9e489e527e
Frode Sjovatsen <frode@fintlabs.no>
2020-10-25 13:35:10 +0100

    added custom put controller

--- src/main/java/no/fint/consumer/models/drosjeloyve/DrosjeloyveCustomController.java
+++ src/main/java/no/fint/consumer/models/drosjeloyve/DrosjeloyveCustomController.java
@@ -6,6 +6,7 @@ import no.fint.model.resource.arkiv.samferdsel.DrosjeloyveResource;
 import no.fint.relations.FintRelationsMediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,12 +17,23 @@ public class DrosjeloyveCustomController {
     private DrosjeloyveController sakController;
 
     @GetMapping("/mappeid/{ar}/{sekvensnummer}")
-    public DrosjeloyveResource getSakByMappeArSekvensnummer(
+    public DrosjeloyveResource getDrosjeloyveByMappeArSekvensnummer(
             @PathVariable String ar,
             @PathVariable String sekvensnummer,
             @RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId,
             @RequestHeader(name = HeaderConstants.CLIENT, required = false) String client) throws InterruptedException {
         return sakController.getDrosjeloyveByMappeId(ar + "/" + sekvensnummer, orgId, client);
+    }
+
+    @PutMapping("/mappeid/{ar}/{sekvensnummer}")
+    public ResponseEntity updateDrosjeloyveByMappeArSekvensnummer(
+            @PathVariable String ar,
+            @PathVariable String sekvensnummer,
+            @RequestHeader(name = HeaderConstants.ORG_ID, required = false) String orgId,
+            @RequestHeader(name = HeaderConstants.CLIENT, required = false) String client,
+            @RequestBody DrosjeloyveResource body
+    ) {
+        return sakController.putDrosjeloyveByMappeId(ar + "/" + sekvensnummer, orgId, client, body);
     }
 
 }
