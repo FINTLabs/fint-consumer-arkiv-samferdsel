825a4a124607b1904d3cce6867d9d379f7821947
Sander Nilsen Rysjedal <Sander.Rysjedal@vigoiks.no>
2026-01-14 13:13:21 +0100

    Version 4.0.0-rc-3.

--- src/main/java/no/fint/consumer/utils/RestEndpoints.java
+++
@@ -1,9 +0,0 @@
-package no.fint.consumer.utils;
-
-public enum RestEndpoints {
-    ;
-
-    public static final String ADMIN = "/admin";
-	public static final String SOKNADDROSJELOYVE = "/soknaddrosjeloyve";
-
-}
